193c603c2c660ac0a4bb06347ddc9ff1fd746c41
rishika mali <rishikamali383@gmail.com>
2026-02-14 20:19:54 +0530

    todo.py
    This is a basic To-Do List app built with Python and Tkinter.
    It allows users to add, delete, and save tasks. Tasks are stored in a file and automatically loaded when the app restarts.
    
    This project helped me learn GUI design and file handling in Python.

--- todo.py
+++ todo.py
@@ -1,85 +1,87 @@
 import tkinter as tk
 from tkinter import messagebox
+import os
 
 root = tk.Tk()
-root.title(" My To-Do List 📝")
-root.geometry("400x500")
-root.config(bg="#1b505e")  
+root.title("My To-Do List 📝")
+root.geometry("400x520")
+root.config(bg="#1b505e")
 
-tasks = []
+FILE_NAME = "tasks.txt"
+
+def save_tasks():
+    tasks = listbox.get(0, tk.END)
+    with open(FILE_NAME, "w", encoding="utf-8") as file:
+        for task in tasks:
+            file.write(task + "\n")
+    print("Tasks Saved!")   # check terminal
+
+
+def load_tasks():
+    if os.path.exists(FILE_NAME):
+        with open(FILE_NAME, "r", encoding="utf-8") as file:
+            for line in file:
+                listbox.insert(tk.END, line.strip())
+
 
 def add_task():
-  task = entry.get()
-
-  if task != "":
-   listbox.insert(tk.END, "✨ " + task)
-   entry.delete(0, tk.END)
-  else:
-     messagebox.showwarning("Warning", "Please enter a task!")
+    task = entry.get().strip()
+    if task:
+        listbox.insert(tk.END, "➡ " + task)
+        entry.delete(0, tk.END)
+        save_tasks()   # auto save
+    else:
+        messagebox.showwarning("Warning", "Please enter a task!")
 
 def delete_task():
     try:
         selected = listbox.curselection()[0]
         listbox.delete(selected)
- 
+        save_tasks()
     except:
-
-     messagebox.showwarning("Warning", "Please select a task!")
+        messagebox.showwarning("Warning", "Select a task!")
 
 def clear_tasks():
     listbox.delete(0, tk.END)
+    save_tasks()
 
-def on_enter(e):
-  e.widget.config(bg="#fafafa")
+def on_closing():
+    save_tasks()
+    root.destroy()
 
+tk.Label(root, text="MY TO-DO LIST 📝",
+         font=("Arial", 20, "bold"),
+         bg="#1b505e", fg="black").pack(pady=10)
 
-title = tk.Label(root, text=" MY TO-DO LIST 📝",
-                font=("Arial", 20, "bold"),
-                bg="#1b505e", fg="#000405")
-title.pack(pady=10)
-
-
-entry = tk.Entry(root, font=("Arial", 20), width=25)
+entry = tk.Entry(root, font=("Arial", 18), width=25)
 entry.pack(pady=10)
 
+frame = tk.Frame(root, bg="#1b505e")
+frame.pack(pady=10)
 
-button_frame = tk.Frame(root, bg="#1b505e")
-button_frame.pack(pady=10)
+tk.Button(frame, text="Add Task", bg="#31f50a",
+          command=add_task).pack(side=tk.LEFT, padx=5)
 
-add_btn = tk.Button(button_frame, text="Add Task", 
-                font=("Arial", 12),
-                bg="#31f50a",
-                fg="black",
-                command=add_task)
-add_btn.pack(side=tk.LEFT, padx=10)
+tk.Button(frame, text="Delete Task", bg="#f50a0a",
+          command=delete_task).pack(side=tk.LEFT, padx=5)
 
-delete_btn = tk.Button(button_frame, text="Delete Task",
-                   font=("Arial", 12),
-                   bg="#f50a0a",
-                   fg="black",
-                   command=delete_task)
-delete_btn.pack(side=tk.LEFT, padx=10)
+tk.Button(frame, text="Clear All", bg="#f5a60a",
+          command=clear_tasks).pack(side=tk.LEFT, padx=5)
 
-clear_btn = tk.Button(button_frame, text="Clear All",
-                  font=("Arial", 12),
-                  bg="#f5a60a",
-                  fg="black",
-                  command=clear_tasks)
-clear_btn.pack(side=tk.LEFT, padx=10)
+tk.Button(root, text="💾 Save Tasks",
+          bg="#00c2ff",
+          command=save_tasks).pack(pady=5)
+
+listbox = tk.Listbox(root, font=("Arial", 16),
+                     width=30, height=15)
+listbox.pack(pady=10)
+
+relief="flat"
 
 
-listbox = tk.Listbox(root,
-                 font=("Arial", 16),
-                 width=30,
-                 height=20)
-listbox.pack(pady=15)
+load_tasks()
 
+root.protocol("WM_DELETE_WINDOW", on_closing)
 
-def on_leave(e):
-  e.widget.config(bg=e.widget.original_color)
-for btn in [add_btn, delete_btn, clear_btn]:
-   btn.original_color = btn["bg"]
-   btn.bind("<Enter>", on_enter)
-   btn.bind("<Leave>", on_leave)
+root.mainloop()
 
-root.mainloop()  
